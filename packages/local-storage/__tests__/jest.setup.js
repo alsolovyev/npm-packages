0b2c044c2bc068e18538e77692d98edac7e7d663
Aleksey Solovyev <solovyev.a@icloud.com>
2022-12-19 20:53:07 +0300

    feat: set up jest

---
+++ packages/local-storage/__tests__/jest.setup.js
@@ -0,0 +1,34 @@
+window.localStorage = window.localStorage || {}
+
+const options = {
+  writable: false,
+  enumerable: false
+}
+
+Object.defineProperties(window.localStorage, {
+  setItem: {
+    ...options,
+    value: jest.fn((key, val) => {
+      this[key] = String(val)
+    })
+  },
+
+  getItem: {
+    ...options,
+    value: jest.fn(key => (this[key] !== undefined ? this[key] : null))
+  },
+
+  removeItem: {
+    ...options,
+    value: jest.fn(key => {
+      delete this[key]
+    })
+  },
+
+  clear: {
+    ...options,
+    value: jest.fn(() => {
+      Object.keys(this).map(key => delete this[key])
+    })
+  }
+})
